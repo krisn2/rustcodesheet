16207a4d1cfb63b0a3e9e1d3648ff61dc1702ddb
krisn2 <krishnasarone08@gmail.com>
2024-07-14 00:29:01 +0530

    control flow add

---
+++ src/controlflow_mod.rs
@@ -0,0 +1,45 @@
+pub mod conditional {
+    pub fn test_if (){
+        let age_to_drive:u8 = 18u8;
+        
+        println!("Enter your age: ");
+        let mut age = String::new();
+        std::io::stdin().read_line(&mut age).unwrap();
+
+        if age.trim().parse::<u8>().unwrap() >= age_to_drive {
+            println!("You can drive");
+        } else {
+            println!("You cannot drive");
+        }
+
+    }
+}
+
+pub mod loops {
+
+    pub fn test_for(){
+        for mut i in 0..5 {
+            print!("{}",i);
+            i +=1;
+        }
+    }
+
+    pub fn test_while(){
+        let i = 3;
+        while i > 0 {
+            print!("Waiting...;");
+        }
+    }
+
+    pub fn test_loop(){
+        let mut i = 5;
+        loop  {
+            print!("Waiting...;");
+
+            if i == 5 {
+                break;
+            }
+            i += 1;
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-pub mod modules;
 pub mod data_type_mod;
+pub mod controlflow_mod;
 
 fn main() {
+     // All data type in that module
      data_type_mod::data_types();
-     let value_of_fn = modules::get_name("krishn", "sarone");
-     println!("value of fn get name: {}", value_of_fn);
+
+     // All control flow in that module like conditional and loops
+     controlflow_mod::conditional::test_if();
 }
